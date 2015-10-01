3147bc0317d7301961bd246c77b710b0ca4d3a21
Gebhard Wöstemeyer <g.woestemeyer@gmail.com>
2015-10-01 14:23:27 +0200

    html formatted help text

--- app/src/main/java/com/bluebird_tech/puffin/TensionInfoFragment.java
+++ app/src/main/java/com/bluebird_tech/puffin/TensionInfoFragment.java
@@ -5,6 +5,9 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.text.Html;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 /**
  * Created by opahk on 23/09/15.
@@ -12,17 +15,15 @@ import android.support.v7.app.AlertDialog;
 public class TensionInfoFragment extends DialogFragment {
   @Override
   public Dialog onCreateDialog(Bundle savedInstanceState) {
-    // Use the Builder class for convenient dialog construction
     AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
     builder
-      .setTitle(R.string.dialog_title)
-      .setMessage(R.string.dialog_message)
+      .setMessage(
+        Html.fromHtml(getResources().getString(R.string.dialog_message)))
       .setPositiveButton("OK", new DialogInterface.OnClickListener() {
         public void onClick(DialogInterface dialog, int id) {
           // FIRE ZE MISSILES!
         }
       });
-    // Create the AlertDialog object and return it
     return builder.create();
   }
 }
